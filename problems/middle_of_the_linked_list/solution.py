62656ad9005a510f04ce6cce818eaa1f617a9e99
Nick <onyourwill@gmail.com>
2024-08-14 02:14:34 +0000

    Sync LeetCode submission - Middle of the Linked List (python3)

--- problems/middle_of_the_linked_list/solution.py
+++ problems/middle_of_the_linked_list/solution.py
@@ -6,17 +6,9 @@
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         s = head
-        f = head.next
+        f = head
         while f != None and f.next != None:
             f = f.next.next
             s = s.next
-        if head.next != None:
-            s2 = head.next
-            f2 = head.next.next
-            while f2 != None and f2.next != None:
-                f2 = f2.next.next
-                s2 = s2.next
-            if s != s2:
-                return s2
         return s
         
