213bbdc26fec5dc19547523c91b499fad083e90a
Nick <onyourwill@gmail.com>
2024-08-31 03:26:32 +0000

    Sync LeetCode submission - Trapping Rain Water (python3)

--- problems/trapping_rain_water/solution.py
+++ problems/trapping_rain_water/solution.py
@@ -1,15 +1,16 @@
-
 class Solution:
+    # 2024-08-31
     def trap(self, height: List[int]) -> int:
-        n = len(height)
         ans = 0
+        n = len(height)
         if n < 2:
             return ans
         stack = []
         for i in range(n):
+            
             while stack and height[i] > height[stack[-1]]:
-                top = stack.pop()
-                while stack and height[top] == height[stack[-1]]:
+                top = stack[-1]
+                while stack and height[stack[-1]] == height[top]:
                     stack.pop()
                 if stack:
                     left = stack[-1]
