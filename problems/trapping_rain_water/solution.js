497d623080a7ebe9a8d89c7b7bc924e846c68911
Nick <onyourwill@gmail.com>
2024-09-03 09:25:04 +0000

    Sync LeetCode submission - Trapping Rain Water (javascript)

--- problems/trapping_rain_water/solution.js
+++ problems/trapping_rain_water/solution.js
@@ -3,22 +3,20 @@
  * @return {number}
  */
 var trap = function(height) {
-    const len = height.length
-    let ans = 0
-    if (len < 2) return ans
-    let stack = []
-    for(let i = 0; i < len; i++) {
-        while(stack.length > 0 && height[i] > height[stack[stack.length - 1]]) {
-            const top = stack[stack.length - 1]
-            while(stack.length > 0 && height[top] == height[stack[stack.length - 1]]) {
-                stack.pop()
-            }
-            if (stack.length > 0) {
-                left = stack[stack.length - 1]
-                ans += (Math.min(height[left], height[i]) - height[top]) * (i - left -1)
-            }
+    let sum=0,l=0,r=height.length-1;
+    let maxl=0,maxr=0;
+    while(l<r){
+        maxl = Math.max(height[l],maxl);
+        maxr = Math.max(height[r],maxr);
+        
+        if(maxl === Math.min(maxl,maxr)){
+            sum += maxl - height[l];
+            l++;
         }
-        stack.push(i)
+        else{
+            sum += maxr - height[r];
+            r--;
+        }
     }
-    return ans
+    return sum;
 };
