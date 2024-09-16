56bf46051e569412aa9507f607f8b8c489fb71d4
Nick <onyourwill@gmail.com>
2024-09-16 06:59:10 +0000

    Sync LeetCode submission - Count Number of Nice Subarrays (python3)

--- problems/count_number_of_nice_subarrays/solution.py
+++ problems/count_number_of_nice_subarrays/solution.py
@@ -1,13 +1,13 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        def atMost(k):
-            count = ans = left = 0
+        def atMostOddIn(k):
+            ans = cnt = left = 0
             for right in range(len(nums)):
-                count += nums[right] % 2
-                while count > k:
-                    count -= nums[left] % 2
+                cnt += nums[right] % 2
+                while cnt > k:
+                    cnt -= nums[left] % 2
                     left += 1
                 ans += right - left + 1
             return ans
+        return atMostOddIn(k) - atMostOddIn(k - 1)
         
-        return atMost(k) - atMost(k - 1)
