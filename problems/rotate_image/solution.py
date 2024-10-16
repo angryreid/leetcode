cc286df804fb6eb458f662a5dcf2601ccdb230ee
Nick <onyourwill@gmail.com>
2024-10-16 02:14:37 +0000

    Sync LeetCode submission - Rotate Image (python3)

--- problems/rotate_image/solution.py
+++ problems/rotate_image/solution.py
@@ -3,19 +3,15 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        # n = len(matrix)
-        # for i in range(n // 2):
-        #     for j in range(i, n - 1 - i):
-        #         t = matrix[i][j]
-        #         matrix[i][j] = matrix[n - 1 - j][i]
-        #         matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
-        #         matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
-        #         matrix[j][n - 1 - i] = t
-
         n = len(matrix)
-        for i in range(n//2):
-            matrix[i],matrix[n-i-1] = matrix[n-i-1],matrix[i]
-        
-        for i in range(n):
-            for j in range(i):
-                matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
+        for i in range(n // 2):
+            for j in range(i, n - i - 1):
+                t = matrix[i][j]
+                # left top
+                matrix[i][j] = matrix[n - j - 1][i]
+                # left bottom
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
+                # right bottom
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
+                # right top
+                matrix[j][n - i - 1] = t
