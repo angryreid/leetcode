62f7ff925a2f09691c8c6724b9895c7b7abe7f80
Nick <onyourwill@gmail.com>
2024-10-26 07:59:41 +0000

    Sync LeetCode submission - Pow(x, n) (python3)

--- problems/pow(x,_n)/solution.py
+++ problems/pow(x,_n)/solution.py
@@ -2,4 +2,13 @@ class Solution:
     def myPow(self, x: float, n: int) -> float:
         # ans = x
         # for i in range(n)
-        return x ** n
+        is_neg = n < 0
+        n = abs(n)
+        mult = 1
+        while n > 0:
+            if n % 2 == 0:
+                x *= x
+                n = n // 2
+            n -= 1
+            mult *= x
+        return mult if not is_neg else 1 / mult
