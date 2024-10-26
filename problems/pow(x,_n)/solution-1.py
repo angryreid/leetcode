d48b3d6d7add1d56afeb103999408b67f9f3b125
angryreid <onyourwill@gmail.com>
2024-10-26 16:03:56 +0800

    Add iterative solution for pow(x, n) and improve existing implementation

---
+++ problems/pow(x,_n)/solution-1.py
@@ -0,0 +1,5 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        # ans = x
+        # for i in range(n)
+        return x ** n
--- problems/pow(x,_n)/solution.py
+++ problems/pow(x,_n)/solution.py
@@ -1,14 +1,19 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        # ans = x
-        # for i in range(n)
+        # Check if the exponent n is negative
         is_neg = n < 0
+        # Take the absolute value of n to simplify calculations
         n = abs(n)
+        # Initialize the result multiplier to 1
         mult = 1
+        # Loop until n becomes 0
         while n > 0:
+            # If n is even, square the base x and halve the exponent n
             if n % 2 == 0:
                 x *= x
                 n = n // 2
+            # If n is odd, decrement n and multiply the result by x
             n -= 1
             mult *= x
+        # If the original exponent was negative, return the reciprocal of the result
         return mult if not is_neg else 1 / mult
