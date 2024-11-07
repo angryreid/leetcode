ce7406aa98a4bad094416e2d3465de6be26baef5
Nick <onyourwill@gmail.com>
2024-11-07 14:38:44 +0000

    Sync LeetCode submission - Maximum Average Subarray I (python3)

--- problems/maximum_average_subarray_i/solution.py
+++ problems/maximum_average_subarray_i/solution.py
@@ -1,13 +1,8 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        max_avr = float('-inf')
-        sums = 0
-        left = 0
-        for right in range(len(nums)):
-            sums += nums[right]
-            if right - left + 1 == k:
-                max_avr = max(max_avr, sums / k)
-            if right >= k - 1:
-                sums -= nums[left]
-                left += 1
-        return max_avr
+        best = cur = sum(nums[:k])
+        for i in range(k, len(nums)):
+            cur += nums[i] - nums[i - k]
+            if cur > best:
+                best = cur
+        return best / k
