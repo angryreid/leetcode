7c6292aa142637fa1bda97debea9f4c2f138fb03
Nick <onyourwill@gmail.com>
2024-11-19 14:53:36 +0000

    Sync LeetCode submission - Merge k Sorted Lists (python3)

--- problems/merge_k_sorted_lists/solution.py
+++ problems/merge_k_sorted_lists/solution.py
@@ -1,29 +1,38 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        if not lists:return 
-        return self.merge(lists,0,len(lists) - 1)
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists: return
+        return self.merge(lists, 0, len(lists) - 1)
     
-    def merge(self,lists,left , right ):
-        if left == right : 
+    def merge(self, lists, left, right):
+        if left == right:
             return lists[left]
         mid = left + (right - left) // 2
         l1 = self.merge(lists, left, mid)
-        l2 = self.merge(lists, mid+1, right)
-        return self.mergeTwoLists(l1,l2)
-
+        l2 = self.merge(lists, mid + 1, right)
+        return self.mergeTwoLists(l1, l2)
+    
     def mergeTwoLists(self, l1, l2):
-        dummy = ListNode(-1)
+        dummy = ListNode()
         pre = dummy
+
         while l1 and l2:
-            if l1.val <= l2.val :
+            if l1.val < l2.val:
                 pre.next = l1
                 l1 = l1.next
             else:
-                pre.next =l2
+                pre.next = l2
                 l2 = l2.next
             pre = pre.next
-        if l1 != None :
+        
+        if l1:
             pre.next = l1
-        if l2 != None :
-            pre.next = l2;
+
+        if l2:
+            pre.next = l2
+        
         return dummy.next
