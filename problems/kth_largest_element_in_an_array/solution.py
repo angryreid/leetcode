1444039bb89d7edb129cc4a64a1f4139f29c3766
angryreid <onyourwill@gmail.com>
2025-02-13 22:24:52 +0800

    Improve findKthLargest function with additional comments and handling for empty input

--- problems/kth_largest_element_in_an_array/solution.py
+++ problems/kth_largest_element_in_an_array/solution.py
@@ -1,16 +1,32 @@
 class Solution:
     def findKthLargest(self, nums, k):
+        # If the list is empty, return None
         if not nums: return
+        
+        # Choose a random pivot element from the list
         pivot = random.choice(nums)
-        left =  [x for x in nums if x > pivot]
-        mid  =  [x for x in nums if x == pivot]
+        
+        # Partition the list into three parts:
+        # 'left' contains elements greater than the pivot
+        left = [x for x in nums if x > pivot]
+        # 'mid' contains elements equal to the pivot
+        mid = [x for x in nums if x == pivot]
+        # 'right' contains elements less than the pivot
         right = [x for x in nums if x < pivot]
         
+        # Calculate the lengths of the 'left' and 'mid' lists
         L, M = len(left), len(mid)
         
+        # If k is less than or equal to the length of 'left',
+        # the k-th largest element is in the 'left' list
         if k <= L:
             return self.findKthLargest(left, k)
+        # If k is greater than the length of 'left' plus the length of 'mid',
+        # the k-th largest element is in the 'right' list
+        # Adjust k by subtracting the lengths of 'left' and 'mid'
         elif k > L + M:
             return self.findKthLargest(right, k - L - M)
+        # If k is within the range of the 'mid' list,
+        # the k-th largest element is the pivot (any element in 'mid')
         else:
             return mid[0]
