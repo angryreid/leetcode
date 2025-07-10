acc017cc1da56c4af5e8afb57fae4d8600445b2e
Nick <onyourwill@gmail.com>
2025-07-10 04:13:44 +0000

    Sync LeetCode submission - Combination Sum II (python3)

--- problems/combination_sum_ii/solution.py
+++ problems/combination_sum_ii/solution.py
@@ -7,16 +7,16 @@ class Solution:
         def backtrack(remaining, level):
             if remaining < 0:
                 return
-            
             if remaining == 0:
                 all_list.append(cur_list.copy())
-                return
-            
+
             for i in range(level, len(candidates)):
                 if i > level and candidates[i] == candidates[i - 1]:
                     continue
                 cur_list.append(candidates[i])
                 backtrack(remaining - candidates[i], i + 1)
                 cur_list.pop()
-        backtrack(target, 0)    
+            
+        
+        backtrack(target, 0)
         return all_list
