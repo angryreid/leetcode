0f6cb263c3861bb236cd5116015add743ccb093f
Nick <onyourwill@gmail.com>
2025-07-10 04:07:52 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (python3)

--- problems/letter_combinations_of_a_phone_number/solution.py
+++ problems/letter_combinations_of_a_phone_number/solution.py
@@ -3,25 +3,24 @@ class Solution:
         res = []
         if len(digits) == 0:
             return res
-        
-        phoneMap = {
-            "2":"abc",
-            "3":"def",
-            "4":"ghi",
-            "5":"jkl",
-            "6":"mno",
-            "7":"pqrs",
-            "8":"tuv",
-            "9":"wxyz"
+        num_map = {
+            '2': 'abc',
+            '3': 'edf',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
         }
+
         def dfs(index, c):
             if index == len(digits):
                 res.append(c)
                 return
             digit = digits[index]
-            letters = phoneMap[digit]
+            letters = num_map[digit]
             for lt in letters:
                 dfs(index + 1, c + lt)
-            
         dfs(0, '')
         return res
