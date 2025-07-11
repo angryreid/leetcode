9c2f5472130c4ce6bbd264a19f02de0cc7aa7361
Nick <onyourwill@gmail.com>
2025-07-11 01:32:41 +0000

    Sync LeetCode submission - Jump Game (python3)

--- problems/jump_game/solution.py
+++ problems/jump_game/solution.py
@@ -1,17 +1,10 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        n = len(nums)
-        jump = [0] * n
-        for i in range(n):
-            jump[i] = i + nums[i]
+        max_reach = 0
+        for i, num in enumerate(nums):
+            if i > max_reach:
+                return False
+            max_reach = max(max_reach, i + nums[i])
 
-        i = 0
-        max_jump = jump[0]
-        while i < n and i <= max_jump:
-            if jump[i] > max_jump:
-                max_jump = jump[i]
-            i += 1
-        if i >= n:
-            return True
-        
-        return False
+
+        return True
