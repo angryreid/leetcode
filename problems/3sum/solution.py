3ac9f35a9346b8d829bd0b1ff746823389b1a3f6
Nick <onyourwill@gmail.com>
2025-07-11 06:58:10 +0000

    Sync LeetCode submission - 3Sum (python3)

--- problems/3sum/solution.py
+++ problems/3sum/solution.py
@@ -1,32 +1,29 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        ans = []
-        n = len(nums)
         nums.sort()
-        target = 0
+        n = len(nums)
+        res = []
         for i in range(n - 2):
+            if nums[i] + nums[i + 1] + nums[i + 2] > 0:
+                break
+            if nums[i] + nums[n - 2] + nums[n - 1] < 0:
+                continue
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-
-            if nums[i] + nums[i + 1] + nums[i + 2] > target:
-                break
-
-            if nums[i] + nums[n - 2] + nums[n - 1] < target:
-                continue
-            left, right = i + 1, n - 1
-            while left < right:
-                sum = nums[i] + nums[left] + nums[right]
-                if sum == target:
-                    ans.append([nums[i], nums[left], nums[right]])
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-                    left += 1
-                    right -= 1
-                elif sum > target:
-                    right -= 1
+            
+            l, r = i + 1, n - 1
+            while l < r:
+                sum = nums[i] + nums[l] + nums[r]
+                if sum == 0:
+                    res.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l + 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r - 1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+                elif sum > 0:
+                    r -= 1
                 else:
-                    left += 1
-        return ans
-        
+                    l += 1
+        return res
