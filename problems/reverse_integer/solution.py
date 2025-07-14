b581782f5ca1d1c64a85215e381b92765d70c988
Nick <onyourwill@gmail.com>
2025-07-14 15:09:00 +0000

    Sync LeetCode submission - Reverse Integer (python3)

--- problems/reverse_integer/solution.py
+++ problems/reverse_integer/solution.py
@@ -1,14 +1,13 @@
 class Solution:
     def reverse(self, x: int) -> int:
+        INT_MAX = 2**31
         sign = -1 if x < 0 else 1
-        x *= sign
         res = 0
-        bound = (2**31 - 1) // 10
-        while x:
+        x = abs(x)
+        while x != 0:
             pop = x % 10
-            x //= 10
-            if res > bound or (res == bound and pop > 7):
+            x = x // 10
+            if res > (INT_MAX - pop) // 10:
                 return 0
             res = res * 10 + pop
         return sign * res
-
