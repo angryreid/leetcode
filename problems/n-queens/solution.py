d4248f5fc24cacb5abd4e0cc7d02687aaca947bb
Nick <onyourwill@gmail.com>
2025-07-16 13:35:38 +0000

    Sync LeetCode submission - N-Queens (python3)

--- problems/n-queens/solution.py
+++ problems/n-queens/solution.py
@@ -2,20 +2,19 @@ class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         res = []
         board = [0] * n
-        def is_safe(board, row, col):
+        def is_valid(board, row, col):
             for i in range(row):
                 if board[i] == col or abs(board[i] - col) == row - i:
                     return False
             return True
-        def solve_n_queen(board, row):
+        
+        def backtrack(board, row):
             if row == n:
                 res.append(['.' * i + 'Q' + '.' * (n - i - 1) for i in board])
                 return
             for col in range(n):
-                if is_safe(board, row, col):
+                if is_valid(board, row, col):
                     board[row] = col
-                    solve_n_queen(board, row + 1)
-        
-        solve_n_queen(board, 0)
+                    backtrack(board, row + 1)
+        backtrack(board, 0)
         return res
-                
