87570ffda667fcb5c180575363bee7178fbdcc74
Nick <onyourwill@gmail.com>
2025-07-20 07:30:35 +0000

    Sync LeetCode submission - Island Perimeter (python3)

---
+++ problems/island_perimeter/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        perimeter = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    perimeter += 4
+                    # # top
+                    # if i > 0 and grid[i - 1][j] == 1:
+                    #     perimeter -= 2
+                    # # left
+                    # if j > 0 and grid[i][j - 1] == 1:
+                    #     perimeter -= 2
+                    if i < m - 1 and grid[i + 1][j] == 1:
+                        perimeter -= 2
+                    if j < n - 1 and grid[i][j + 1] == 1:
+                        perimeter -= 2
+        return perimeter
